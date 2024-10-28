1ffb8251e8e90783c30fca715ed50c10f824fd24
Bhushith Gujjala Hari <87918619+teddyboy999@users.noreply.github.com>
2024-10-29 08:25:07 +0900

    Updated Python Files
    main.py is used to pass the data through the linear regression model.
    helper.py has all the math methods needed.

---
+++ helper.py
@@ -0,0 +1,26 @@
+# Helper Module
+# Has Math functions such as variance(X), Standard Deviation(X), etc.
+
+# Testing Method
+def printHello():
+	print("hello world!")
+
+
+'''
+Define the following functions:
+'''
+
+def mean(arrX):
+	print()
+
+
+def variance(arrX):
+	print()
+
+
+def covariance(arrX, arrY):
+	print()
+
+
+def coefficientsFunc(matrixA):
+	print()
---
+++ main.py
@@ -0,0 +1,10 @@
+# main
+
+# Imports
+# Import the helper.py file with all the math functions
+import helper
+
+if (__name__ == "__main__"):
+	
+	# Using methods from helper.py
+	helper.printHello()
